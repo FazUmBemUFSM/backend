e966393dcbca5c5a6c9a76a4ad120a9a4c66ab92
BrunoBudelRossi <bbrossi@inf.ufsm.br>
2021-08-29 18:17:48 -0300

    feat: change configs for deploy api on heroku.

--- src/db/database.js
+++ src/db/database.js
@@ -3,19 +3,9 @@ module.exports = {
         url: process.env.DATABASE_URL,
     },
     test: {
-        username: 'root',
-        password: null,
-        database: 'fazumbem',
-        host: '127.0.0.1',
-        dialect: 'postgres',
-        schema: 'public',
+        url: process.env.DATABASE_URL,
     },
     production: {
-        username: 'root',
-        password: null,
-        database: 'fazumbem',
-        host: '127.0.0.1',
-        dialect: 'postgres',
-        schema: 'public',
+        url: process.env.DATABASE_URL,
     },
 };
